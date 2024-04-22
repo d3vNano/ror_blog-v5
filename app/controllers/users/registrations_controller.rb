1b6419522ca208617ba55b3978c8b1cff07c9581
d3vNano <contato.dorigo@gmail.com>
2024-04-22 17:10:34 -0300

    fix: edit user name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :password, :password_confirmation, :current_password])
+    end
+
     def sign_up_params
        params.require(:user).permit(:name, :pin, :email, :password, :password_confirmation)
     end
