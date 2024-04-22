e419e010a896eae71110fccc4ae1f4fb2fe2174b
d3vNano <contato.dorigo@gmail.com>
2024-04-22 17:49:44 -0300

    ...

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+window.$ = window.jQuery = $;
+window.Popper = Popper;
